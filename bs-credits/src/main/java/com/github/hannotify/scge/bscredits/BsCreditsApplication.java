c8d4ae6f9def087ec29db530354afadda8c2da5f
Hanno Embregts <hanno.embregts@infosupport.com>
2020-06-25 20:28:26 +0200

    Register services at Eureka Server

--- bs-credits/src/main/java/com/github/hannotify/scge/bscredits/BsCreditsApplication.java
+++ bs-credits/src/main/java/com/github/hannotify/scge/bscredits/BsCreditsApplication.java
@@ -2,8 +2,10 @@ package com.github.hannotify.scge.bscredits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BsCreditsApplication {
 
 	public static void main(String[] args) {
--- bs-intro/src/main/java/com/github/hannotify/scge/bsintro/BsIntroApplication.java
+++ bs-intro/src/main/java/com/github/hannotify/scge/bsintro/BsIntroApplication.java
@@ -2,8 +2,10 @@ package com.github.hannotify.scge.bsintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BsIntroApplication {
 
 	public static void main(String[] args) {
