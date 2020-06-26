9cb35bb1e2d27bc28524937361620b713bfabded
Hanno Embregts <hanno.embregts@infosupport.com>
2020-06-26 09:32:52 +0200

    Change port to demonstrate load balancing

--- bs-intro/src/main/java/com/github/hannotify/scge/bsintro/IntroController.java
+++ bs-intro/src/main/java/com/github/hannotify/scge/bsintro/IntroController.java
@@ -1,6 +1,7 @@
 package com.github.hannotify.scge.bsintro;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("intro")
 public class IntroController {
+    @Value("${server.port}")
+    private String serverPort;
 
     @GetMapping
     public List<String> getIntro() {
         return List.of(
                 "The Ultimate Olympic Mission",
-                "a film by Tom Kostner"
+                "a film by Tom Kostner",
+                "produced by service on port " + serverPort
         );
     }
 }
