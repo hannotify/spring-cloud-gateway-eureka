c323dfb9c0e3354bba962e1eb2386c89d2eb813b
Hanno Embregts <hanno.embregts@infosupport.com>
2020-06-26 08:47:05 +0200

    Add aggregate routes

--- api-gateway/src/main/java/com/github/hannotify/scge/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/github/hannotify/scge/apigateway/ApiGatewayConfiguration.java
@@ -1,9 +1,11 @@
 package com.github.hannotify.scge.apigateway;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApiGatewayConfiguration {
@@ -17,5 +19,11 @@ public class ApiGatewayConfiguration {
                         .path("/intro")
                         .uri("lb://BS-INTRO"))
                 .build();
+    }
+
+    @Bean("LoadBalanced")
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
     }
 }
---
+++ api-gateway/src/main/java/com/github/hannotify/scge/apigateway/IntroCreditsController.java
@@ -0,0 +1,42 @@
+package com.github.hannotify.scge.apigateway;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+@RequestMapping("intro-credits")
+public class IntroCreditsController {
+    private final WebClient.Builder webclient;
+
+    @Autowired
+    public IntroCreditsController(@Qualifier("LoadBalanced") WebClient.Builder webclient) {
+        this.webclient = webclient;
+    }
+
+    @GetMapping
+    public List<String> getIntroCredits() throws ExecutionException, InterruptedException {
+        List<String> introList = Arrays.asList(
+                Objects.requireNonNull(webclient.build().get().uri("lb://bs-intro/intro")
+                        .retrieve().bodyToMono(String[].class).toFuture().get()));
+
+        List<String> creditsList = Arrays.asList(
+                Objects.requireNonNull(webclient.build().get().uri("lb://bs-credits/credits")
+                        .retrieve().bodyToMono(String[].class).toFuture().get()));
+
+        return Stream.of(introList, creditsList)
+                .flatMap(List::stream)
+                .filter(s -> !s.contains("diagrams")) // only include real work
+                .collect(Collectors.toList());
+    }
+}
